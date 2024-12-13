031e99d4a283335b5af6802653dbf03a9cadad0d
CJ Quines <cj@cjquines.com>
2024-12-13 13:22:20 -0500

    test

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+pnpm turbo prebuild --filter=app-a --summarize --dry=json
+
+pnpm turbo prune app-a
+cd out
+pnpm install
+pnpm turbo prebuild --filter=app-a --summarize --dry=json
